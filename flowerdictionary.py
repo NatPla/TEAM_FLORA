a45e6c5050ddae07aba37f7f810121cad9b19d8b
NatPla <47829190+NatPla@users.noreply.github.com>
2019-06-23 18:29:51 +0100

    edited syntax

--- flowerdictionary.py
+++ flowerdictionary.py
@@ -1,13 +1,14 @@
-rose = {'colour': 'red','size': 'medium', 'smell': ['sweet', 'fragrant'], 'season': ['June', 'July', 'August'], 'Latin_name': 'rosa macdub', 'maintenance': 'high'}
+Rose = {'colour': 'red','size': 'medium', 'smell': ['sweet', 'fragrant'], 'season': ['June', 'July', 'August'], 'Latin_name': 'rosa macdub', 'maintenance': 'high'}
 
 Allium = {'colour': 'purple', 'size': 'large', 'smell': ['onions', 'fragrant'], 'season': ['June'], 'latin name': 'allium sativum', 'maintenance': 'low'}
 
-Oriental_Lily = {'colour': ['pink', 'white'], 'size': 'large', 'smell': ['sweet', 'fragrant'], 'season': ['August', 'September'], 'latin_name': 'lilium ornamentalis', 'low'}
+Oriental_Lily = {'colour': ['pink', 'white'], 'size': 'large', 'smell': ['sweet', 'fragrant'], 'season': ['August', 'September'], 'latin_name': 'lilium ornamentalis', 'maintenance': 'low'}
 
 Peruvian_Lily = {'colour': ['orange', 'yellow'], 'size': 'medium', 'smell': 'none', 'season': ['June', 'July', 'August', 'September'], 'latin_name': 'alstreomeria', 'maintenance': 'low'}
 
 Athene_Amaryllis = {'colour': ['white', 'yellow', 'green'], 'size': 'large', 'smell': ['mild', 'delicate'], 'season': ['December', 'January', 'October', 'November', 'September'], 'latin_name': 'hippeastrum athene', 'maintenance': 'low'}
 
 Bearded_Iris = {'colour': ['white', 'purple'], 'size': 'medium', 'smell': ['mild', 'violets', 'grapes'], 'season': ['May', 'June', 'July', 'August', 'September'], 'latin_name': 'iris germanica', 'maintenance': 'medium'}
+
 
 print(Athene_Amaryllis.get('latin_name', 'Not in season, please try a different search'))
