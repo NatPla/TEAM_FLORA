a2b9f49c405cd5a1443aaee2d488ae48bfa7e69f
NatPla <47829190+NatPla@users.noreply.github.com>
2019-06-23 17:44:51 +0100

    working on changes to dictionary

---
+++ flowerdictionary.py
@@ -0,0 +1,15 @@
+rose = {'colour': 'red', 
+'size': 'large' 
+'smell': ['sweet', 'fragrant']
+'season': ['June', 'July', 'August']
+'latin name': 'rosa macdub'
+}
+
+Allium = {'colour': 'purple', 
+'size': 'large' 
+'smell': ['onions', 'fragrant']
+'season': ['June']
+'latin name': 'allium sativum'
+}
+
+print(student.get('yellow', 'Not in season, please try a different search'))
