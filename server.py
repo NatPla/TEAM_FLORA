a8c0519490459f53211b56e3c2346b2e92d221e1
NatPla <47829190+NatPla@users.noreply.github.com>
2019-06-25 18:31:50 +0100

    fixed search terms
    can now search for smells

--- server.py
+++ server.py
@@ -22,16 +22,9 @@ def search():
   form_data = request.form # request.form is used to assign data passed in through search to form_data. In this case a flower is expected.
   flower_being_searched_for = form_data["flower"] # Find the desired flower enetry from form_data and assign it to flower_being_searched_for
   # Given the flower, we want to find all relevant information to said flower.
-  flower_feature_list = return_all_flower_features([size_dictionary, colour_dictionary, smell_dictionary, season_dictionary, latin_dictionary, maintenance_dictionary],flower_being_searched_for)
+  flowers = find_feature(smell_dictionary,flower_being_searched_for)
   # Following depends on featurs being returned in right order. If any flower does not have an entry in one of the dictionaries, or multiple, things will go very wrong.
   #  The .HTML file is not set up to use these things, but these strings should be passed in to it from here.
-  return render_template("flower.html", 
-  FLOWER_NAME = form_data["flower"],
-  FLOWER_SIZE = flower_feature_list[0], 
-  FLOWER_COLOUR = flower_feature_list[1],
-  FLOWER_SMELL = flower_feature_list[2],
-  FLOWER_SEASON = flower_feature_list[3],
-  FLOWER_LATIN_NAME = flower_feature_list[4]
-  )
+  return render_template("flower.html", flower=flowers)
 
 app.run()
